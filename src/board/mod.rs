40d845d5f347ee9342b198fcdfe30c1ac9218898
afaucher17 <aymeric.faucher@gmail.com>
2016-05-29 13:56:00 +0200

    board: impl Display

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -1,9 +1,11 @@
+use std::fmt;
+
 #[derive(Clone)]
 pub enum Square
 {
     Black,
     White,
-    None,
+    Empty,
 }
 
 #[derive(Clone)]
@@ -11,3 +13,19 @@ pub struct Board
 {
     pub state: Vec<Vec<Square>>,
 }
+
+impl fmt::Display for Board
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+    {
+        write!(f, "{}", self.state.iter()
+               .map(|line| line.iter()
+                    .map(|square| match *square {
+                        Square::Empty => " ",
+                        Square::Black => "B",
+                        Square::White => "W"
+                    }).collect::<String>()
+                ).collect::<String>())
+    }
+}
+
