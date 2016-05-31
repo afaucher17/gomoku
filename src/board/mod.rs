716e8d263fc00619cb56f08dd50abb30b6e74f63
Thomas Dieumegard <tdieumeg@student.42.fr>
2016-05-31 15:34:59 +0200

    board/mod.rs: check victory done for all directions

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -45,13 +45,39 @@ impl Board {
         clone
     }
 
-    pub fn check_victory(&self, color: Square) -> bool {
-        self.state.iter().any(|v| v.iter().group_by(|elt| **elt == color)
-                            .any(|(key, value)| key && value.iter().count() >= 5))
-           || (0..19).any(|i| (0..19)
-                          .map(|j| self.state[i][j].clone())
-                          .collect::<Vec<Square>>().iter()
-                          .group_by(|elt| **elt == color)
-                          .any(|(key, value)| key && value.iter().count() >= 5))
+    pub fn check_aligned(&self, color: Square) -> bool {
+        //  Board line exploration:
+        self.state.iter().any(|v| v.iter()
+                              .group_by(|elt| **elt == color)
+                              //.inspect(|x| println!("{:?}", x))
+                              .any(|(key, value)| key && value.iter().count() >= 5))
+        //  Board column exploration:
+            || (0..19).any(|i| (0..19)
+                           .map(|j| self.state[j][i].clone())
+                           .collect::<Vec<Square>>()
+                           .iter()
+                           .group_by(|elt| **elt == color)
+                           .any(|(key, value)| key && value.iter().count() >= 5))
+        //  Board down-right diagonal exploration:
+            || (0..38).any(|i: i32| (0..19)
+                           .filter(|j: &i32| (i - 19) + j < 19 && (i - 19) + j >= 0)
+                           //.inspect(|j: &i32| println!("[{}][{}]", (i - 19) + j, j))
+                           .map(|j: i32| self.state[((i - 19) + j) as usize][j as usize].clone())
+                           .collect::<Vec<Square>>()
+                           .iter()
+                           .group_by(|elt| **elt == color)
+                           //.inspect(|x| println!("{:?}", x))
+                           .any(|(key, value)| key && value.iter().count() >= 5))
+        //  Board up-right diagonal exploration
+            || (0..38).any(|i: i32| (0..19).rev()
+                           .filter(|j: &i32| i - j < 19 && i - j >= 0)
+                           //.inspect(|j: &i32| println!("[{}][{}]", i - j, j))
+                           .map(|j: i32| self.state[(i - j) as usize][j as usize].clone())
+                           .collect::<Vec<Square>>()
+                           .iter()
+                           .group_by(|elt| **elt == color)
+                           //.inspect(|x| println!("{:?}", x))
+                           .any(|(key, value)| key && value.iter().count() >= 5))
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,42 @@ extern crate gomoku;
 use gomoku::board;
 
 fn main() {
-  let board = board::Board::new();
-  print!("{} {}", board, board.check_victory(board::Square::Black));
+  let mut board = board::Board::new();
+  /*board.state[0][0] = board::Square::Black;
+  board.state[0][1] = board::Square::Black;
+  board.state[0][2] = board::Square::Black;
+  board.state[0][3] = board::Square::Black;
+  board.state[0][4] = board::Square::Black;
+  board.state[2][5] = board::Square::Black;
+  board.state[3][5] = board::Square::Black;
+  board.state[4][5] = board::Square::Black;
+  board.state[5][5] = board::Square::Black;
+  board.state[6][5] = board::Square::Black;*/
+  board.state[0][4] = board::Square::Black;
+  board.state[1][3] = board::Square::Black;
+  board.state[2][2] = board::Square::Black;
+  board.state[3][1] = board::Square::Black;
+  board.state[4][0] = board::Square::Black;
+  println!("{}{}", board, board.check_aligned(board::Square::Black));
+  board = board::Board::new();
+  board.state[14][18] = board::Square::Black;
+  board.state[15][17] = board::Square::Black;
+  board.state[16][16] = board::Square::Black;
+  board.state[17][15] = board::Square::Black;
+  board.state[18][14] = board::Square::Black;
+  println!("{}{}", board, board.check_aligned(board::Square::Black));
+  board = board::Board::new();
+  board.state[0][14] = board::Square::Black;
+  board.state[1][15] = board::Square::Black;
+  board.state[2][16] = board::Square::Black;
+  board.state[3][17] = board::Square::Black;
+  board.state[4][18] = board::Square::Black;
+  println!("{}{}", board, board.check_aligned(board::Square::Black));
+  board = board::Board::new();
+  board.state[14][0] = board::Square::Black;
+  board.state[15][1] = board::Square::Black;
+  board.state[16][2] = board::Square::Black;
+  board.state[17][3] = board::Square::Black;
+  board.state[18][4] = board::Square::Black;
+  println!("{}{}", board, board.check_aligned(board::Square::Black));
 }
