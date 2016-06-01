70afd1b6756113e99a0ab81faf50d92176a2341e
afaucher17 <aymeric.faucher@gmail.com>
2016-06-01 14:47:55 +0200

    board: captures done

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -26,14 +26,14 @@ impl fmt::Display for Board
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
     {
-        write!(f, "{}", self.state.iter()
+        write!(f, "{} Black Captures: {} White Captures: {}", self.state.iter()
                .map(|line| line.iter()
                     .map(|square| match *square {
                         Square::Empty => "_",
                         Square::Black => "B",
                         Square::White => "W"
                     }).collect::<String>() + "\n"
-                   ).collect::<String>())
+                   ).collect::<String>(), self.b_capture, self.w_capture)
     }
 }
 
@@ -53,10 +53,15 @@ impl Board {
         Board { state : vec![vec![Square::Empty; 19]; 19], b_capture : 0, w_capture : 0 }
     }
 
-    pub fn play_at(&self, x: usize, y: usize, color: Square) -> Board {
+    pub fn play_at(&self, x: usize, y: usize, color: &Square) -> Option<Board> {
         let mut clone = self.clone();
-        clone.state[y][x] = color;
-        clone
+        if clone.state[x][y] != Square::Empty {
+            None
+        }
+        else {
+            clone.state[x][y] = color.clone();
+            Some(clone.check_capture(color, (x, y)))
+        }
     }
 
     fn check_capture(&self, color: &Square, pos: (usize, usize)) -> Board {
@@ -65,45 +70,45 @@ impl Board {
         {
             let mut capture = |v: &[(Square, (usize, usize))]| match v {
                 [(ref a, _), (ref b, (xb, yb)), (ref c, (xc, yc)), (ref d, _)] if a == d && b == c && *b == color.opposite() && a == color => {
-                    board.state[xb][yb] = color.clone();
-                    board.state[xc][yc] = color.clone();
+                    board.state[xb][yb] = Square::Empty;
+                    board.state[xc][yc] = Square::Empty;
                     board.b_capture += if *color == Square::Black { 2 } else { 0 };
-                    board.w_capture = if *color == Square::White { 2 } else { 0 };
+                    board.w_capture += if *color == Square::White { 2 } else { 0 };
                 }
                 _ => ()
             };
 
             // East
             if x + 4 < 19 {
-                capture(&(x..x + 5).map(|i| (self.state[i][y].clone(), (i, y))).collect::<Vec<_>>());
+                capture(&(0..4).map(|i| (self.state[x + i][y].clone(), (x + i, y))).collect::<Vec<_>>());
             }
             // West
             if x >= 4 {
-                capture(&(0..x + 1).map(|i| (self.state[x - i][y].clone(), (x - i, y))).collect::<Vec<_>>());
+                capture(&(0..4).map(|i| (self.state[x - i][y].clone(), (x - i, y))).collect::<Vec<_>>());
             }
             // South
             if y + 4 < 19 {
-                capture(&(y..y + 5).map(|i| (self.state[y][i].clone(), (y, i))).collect::<Vec<_>>());
+                capture(&(0..4).map(|i| (self.state[x][y + i].clone(), (x, y + i))).collect::<Vec<_>>());
             }
             // North
             if y >= 4 {
-                capture(&(0..y + 1).map(|i| (self.state[y][x - i].clone(), (y, x - i))).collect::<Vec<_>>());
+                capture(&(0..4).map(|i| (self.state[x][y - i].clone(), (x, y - i))).collect::<Vec<_>>());
             }
             // North-East
             if x + 4 < 19 && y + 4 < 19 {
-                capture(&(0..5).map(|i| (self.state[x + i][y + i].clone(), (x + i, y + i))).collect::<Vec<_>>());
+                capture(&(0..4).map(|i| (self.state[x + i][y + i].clone(), (x + i, y + i))).collect::<Vec<_>>());
             }
             // North-West
             if x + 4 < 19 && y >= 4 {
-                capture(&(0..5).map(|i| (self.state[x + i][y - i].clone(), (x + i, y - i))).collect::<Vec<_>>());
+                capture(&(0..4).map(|i| (self.state[x + i][y - i].clone(), (x + i, y - i))).collect::<Vec<_>>());
             }
             // South-East
             if x >= 4 && y + 4 < 19 {
-                capture(&(0..5).map(|i| (self.state[x - i][y + i].clone(), (x - i, y + i))).collect::<Vec<_>>());
+                capture(&(0..4).map(|i| (self.state[x - i][y + i].clone(), (x - i, y + i))).collect::<Vec<_>>());
             }
             // South-West
             if x >= 4 && y >= 4 {
-                capture(&(0..5).map(|i| (self.state[x - i][y + i].clone(), (x - i, y + i))).collect::<Vec<_>>());
+                capture(&(0..4).map(|i| (self.state[x - i][y - i].clone(), (x - i, y - i))).collect::<Vec<_>>());
             }
         }
         board.clone()
