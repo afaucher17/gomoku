d4ba48250c0aaf16310588235db730f96537daeb
Thomas Dieumegard <tdieumeg@student.42.fr>
2016-06-11 18:10:40 +0200

    trying out things

--- src/minimax/mod.rs
+++ src/minimax/mod.rs
@@ -7,30 +7,31 @@ struct Decision
     chose_move: (usize, usize)
 }
 
-pub fn minimax(board: board::Board, depth: usize, max_depth: usize, alpha: bool, beta: bool, maximizingPlayer: bool) -> usize 
+pub fn minimax(board: board::Board, depth: usize, alpha: i32, beta: i32, maximizingPlayer: bool, prev_play: (usize, usize)) -> usize 
 {
-    if depth == 0 /* or node is a terminal node */ {
+    if depth == 0 || check_full_board(board) || board.check_aligned(prev_play.0, prev_play.1<F15><F15>{
         return board.evaluation(Square::White);
     }
     if maximizingPlayer {
-        let v = usize.min_value();
-        /* for child of board {
-         * let v = cmp::max(v, minimax(child, depth - 1, alpha, beta, false));
-         * let alpha = cmp::max(alpha, v);
-         * if beta <= alpha {
-         *      break; // beta cut-off
-         * }
-         * }
-         * return v; */
+        let mut v = usize.min_value();
+        for child in  {
+            v = cmp::max(v, minimax(child, depth - 1, alpha, beta, false));
+            let alpha = cmp::max(alpha, v);
+            if beta <= alpha {
+                break ; // beta cut-off
+            }
+        }
+        return v;
     }
     else {
-        let v = usize.max_value();
-        /* for child of board {
-         *  let v = cmp::min(v, minimax(child, depth - 1, alpha, beta, true));
-         *  let beta = cmp::min(beta, v);
-         *  if beta <= alpha {
-         *      break; // alpha cut-off
-         *  }
-         *  return v; */
+        let mut v = usize.max_value();
+        for child in board {
+            v = cmp::min(v, minimax(child, depth - 1, alpha, beta, true));
+            let beta = cmp::min(beta, v);
+            if beta <= alpha {
+                break ; // alpha cut-off
+            }
+        }
+        return v;
     }
 }
