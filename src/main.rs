d9906ad2a83d7fb20b24ddc1caa0d0640aee5861
Aymeric FAUCHER <afaucher@e3r11p12.42.fr>
2016-06-25 16:16:46 +0200

    there's a board

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,41 @@
 #[macro_use]
 extern crate gomoku;
+extern crate piston_window;
+
+use piston_window::*;
 use gomoku::board::{Board, Square};
 use gomoku::game;
 
 fn main() {
-/*    for play in board.get_plays(&Square::Black) {
-        println!("({}, {})", play.0 + 1, play.1 + 1);
+    let opengl = OpenGL::V3_2;
+    let mut window: PistonWindow =
+        WindowSettings::new("Gomoku", [1000, 820])
+        .exit_on_esc(true)
+        .opengl(opengl)
+        .build()
+        .unwrap();
+
+    let mut grid = grid::Grid {
+            cols: 19,
+            rows: 19,
+            units: 40.0,
+    };
+
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g| {
+            clear(color::WHITE, g);
+            let transform = c.transform.trans(30.0, 30.0);
+            Rectangle::new([1.0, 0.91, 0.5, 1.0]).
+                draw([0.0, 0.0, 820.0, 820.0],
+                     &c.draw_state,
+                     c.transform,
+                     g
+                     );
+            grid.draw(&Line::new([0.0, 0.0, 0.0, 1.0], 1.0),
+            &c.draw_state,
+            transform,
+            g
+            );
+        })
     }
-    println!("-----------------------------------------------------------");
-    for play in board.get_plays(&Square::White) {
-        println!("<{}, {}>", play.0 + 1, play.1 + 1);
-    }*/
-/*    Board::init_zobrist_array();
-    let board = Board::from(concat!(
-    "_W_________________\n",
-    "_B_________________\n",
-    "_B_________________\n",
-    "___________________\n",
-    "___________________\n",
-    "___________________\n",
-    "___________________\n",
-    "___________________\n",
-    "________B__________\n",
-    "_________B_________\n",
-    "___________________\n",
-    "___________B_______\n",
-    "____________B______\n",
-    "_____________W_____\n",
-    "___________________\n",
-    "WBB________________\n",
-    "__BB_______________\n",
-    "_B_B_______________\n",
-    "W__W_______________"));
-    let board2 = Board::from(concat!(
-    "___________________\n",
-    "___________________\n",
-    "___________________\n",
-    "___________________\n",
-    "___________________\n",
-    "___________________\n",
-    "___________________\n",
-    "___________________\n",
-    "________B__________\n",
-    "_________B_________\n",
-    "__________W________\n",
-    "___________________\n",
-    "___________________\n",
-    "_____________W_____\n",
-    "___________________\n",
-    "___________________\n",
-    "___________________\n",
-    "___________________\n",
-    "___________________"));
-    println!("{:?}\n{:?}", board2, board.play_at(Some((15, 3)), &Square::White));*/
-    game::game_loop(Board::new());    
 }
